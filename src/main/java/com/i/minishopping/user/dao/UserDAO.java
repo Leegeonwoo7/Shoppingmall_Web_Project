e74eef6b3cf1063554fc02f75c775c4cb5707cc4
Leegeonwoo7 <workgeon07@naver.com>
2024-03-14 12:38:26 +0900

    feat: 유저 DAO

---
+++ src/main/java/com/i/minishopping/user/dao/UserDAO.java
@@ -0,0 +1,23 @@
+package com.i.minishopping.user.dao;
+
+import com.i.minishopping.user.bean.UserDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserDAO {
+    void insertUser(UserDTO userDTO); // 사용자를 삽입하는 메소드
+
+    List<UserDTO> getUserList(); // 사용자 목록을 조회하는 메소드
+
+
+
+
+
+    void deleteUser(Long userDTO); // 사용자를 삭제하는 메소드
+
+    UserDTO getUserById(Long userId);
+
+    void updateUser(UserDTO userDTO);
+}
---
+++ src/main/java/com/i/minishopping/user/dao/UserDAOImpl.java
@@ -0,0 +1,40 @@
+package com.i.minishopping.user.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import com.i.minishopping.user.bean.UserDTO;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserDAOImpl implements UserDAO {
+
+    private final SqlSession sqlSession;
+    @Override
+    public void insertUser(UserDTO userDTO) {
+        sqlSession.insert("userSQL.writeUser", userDTO);
+    }
+
+    @Override
+    public List<UserDTO> getUserList() {
+        return sqlSession.selectList("userSQL.getUserList"); // 사용자 목록을 반환 (새로운 리스트로 반환하여 외부에서의 변경 방지)
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        sqlSession.delete("userSQL.deleteUser",userId);
+
+    }
+    @Override
+    public UserDTO getUserById(Long userId) {
+        return sqlSession.selectOne("userSQL.getUserById", userId);
+    }
+
+    @Override
+    public void updateUser(UserDTO userDTO){
+        sqlSession.update("userSQL.updateUser",userDTO);
+    }
+
+}
